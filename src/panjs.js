69ff7488e4b2f958437718cc9efe7bb5310e6166
Philip Knape <philip.knape@gmail.com>
2017-02-20 12:10:01 +0100

    updates code to target wrapper instead of image

--- src/panjs.js
+++ src/panjs.js
@@ -1,32 +1,34 @@
 // @flow
 
-const getParentElement = (type: string = 'width') => (el: EventTarget): number => {
-  const parentRect = el.parentElement;
-  return (parentRect && type) ? parentRect.getBoundingClientRect()[type] : 0;
+import defaults from './defaults';
+
+const getElement = (type: string = 'width') => (el: EventTarget): number => {
+  return (type) ? el.getBoundingClientRect()[type] : el.getBoundingClientRect();
 };
 
-const getParentWidth = getParentElement('width');
-const getParentHeight = getParentElement('height');
-const getParentX = getParentElement('left');
-const getParentY = getParentElement('top');
+const getWidth = getElement('width');
+const getHeight = getElement('height');
+const getX = getElement('left');
+const getY = getElement('top');
 
 const subtract = (el: EventTarget) => (acc: number, val: Function): number => (
   (acc === 0) ? acc + val(el) : acc - val(el)
 );
 
 const calcOffset = (e: MouseEvent, type: string, ...rest: Array<Function>) => (
-  Math.abs(Math.floor(rest.reduce(subtract(e.target), 0) - e[type]))
+  Math.abs(Math.floor(rest.reduce(subtract(e.currentTarget), 0) - e[type]))
 );
 
 const getOffset = (e: MouseEvent): Object => ({
-  x: calcOffset(e, 'clientX', getParentX) / getParentWidth(e.target),
-  y: calcOffset(e, 'clientY', getParentY) / getParentHeight(e.target),
+  x: calcOffset(e, 'clientX', getX) / getWidth(e.currentTarget),
+  y: calcOffset(e, 'clientY', getY) / getHeight(e.currentTarget),
 });
 
-export const sanitizeOffset = (el: HTMLElement, offset: Object): Object => {
+export const sanitizeOffset = (el: HTMLElement, image: HTMLElement, offset: Object): Object => {
+  const { width, height } = image.getBoundingClientRect();
   return {
-    x: (el.getBoundingClientRect().width - getParentWidth(el)) * offset.x,
-    y: (el.getBoundingClientRect().height - getParentHeight(el)) * offset.y
+    x: (width - getWidth(el)) * offset.x,
+    y: (height - getHeight(el)) * offset.y
   };
 };
 
@@ -45,14 +47,23 @@ const panjs = (targets: string | Object, options: Object = {}) => {
   let element = null;
   let offset = {};
 
-  const calcMove = (e) => {
+  // Base configuration for the pinch instance
+  const opts = {...defaults, ...options};
+
+  const calcMove = (e: MouseEvent): void => {
     offset = getOffset(e);
-    moveEl(e.target, sanitizeOffset(e.target, offset));
+    const imageTarget = opts.target ? `img${opts.target}` : 'img';
+    const image = e.currentTarget.querySelector(imageTarget);
+    if (!image) return;
+    moveEl(image, sanitizeOffset(e.currentTarget, image, offset));
   };
 
   const calcMoveResize = (): void => {
     if (!element || offset) return;
-    moveEl(element, sanitizeOffset(element, offset));
+    const imageTarget = opts.target ? `img${opts.target}` : 'img';
+    const image = element.querySelector(imageTarget);
+    if (!image) return;
+    moveEl(image, sanitizeOffset(element, image, offset));
   };
 
   const attachEvents = (el: HTMLElement): void => {
@@ -101,9 +112,6 @@ const panjs = (targets: string | Object, options: Object = {}) => {
     if (element) destroy();
     // dispatchPinchEvent('init', 'before', {});
 
-    // Base configuration for the pinch instance
-    // opts = {...defaults, ...opt};
-
     // resolve target
     // pinchit allows for both a node or a string to be passed
     switch (typeof target) {
@@ -117,7 +125,6 @@ const panjs = (targets: string | Object, options: Object = {}) => {
         element = null;
         console.warn('missing target, either pass an node or a string');
     }
-
     return (element) ? attachEvents(element) : undefined;
 
     // dispatchPinchEvent('init', 'after', {});
