740e1cced22e31035a865b5e382d7bf4f41218c6
Philip Knape <philip.knape@gmail.com>
2017-02-21 17:50:04 +0100

    updates example to reflect reset method

--- docs/app.js
+++ docs/app.js
@@ -8,6 +8,13 @@ panjs('.example-one .img-wrapper', {
   offset: {x: 10, y: 150},
 });
 
+const pan = panjs('.example-two .img-wrapper');
+const resetOne = document.querySelector('.reset-button.base');
+const resetTwo = document.querySelector('.reset-button.next');
+
+resetOne.addEventListener('click', () => pan.reset())
+resetTwo.addEventListener('click', () => pan.reset({ offset: { x: 10, y: 10 }}))
+
 const target = document.querySelector('.event-target');
 const handleEventExample = handleEvent(target);
 const panEvent = panjs('.example-two .img-wrapper');
